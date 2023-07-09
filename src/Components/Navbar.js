3b3c9e1c307c92f5286724c60a5c96be7bce685e
Kdixitji <kdixitji@gmail.com>
2023-07-09 13:38:44 +0530

    Heading file

---
+++ src/Components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../Components/NavbarStyles.css";
+
+const Navbar = () => {
+  return (
+    <div className="header">
+      {/* Heading or Navigation bar */}
+      <div className="heading">Image Generator</div>
+    </div>
+  );
+};
+
+export default Navbar;
